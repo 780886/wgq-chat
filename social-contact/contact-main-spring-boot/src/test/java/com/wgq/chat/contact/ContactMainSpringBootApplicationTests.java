0d3212cbd456682c13d7ed49c2e00838951d4d23
wgq <1351686755@qq.com>
2023-05-30 15:56:21 +0800

    整洁架构im初始

---
+++ social-contact/contact-main-spring-boot/src/test/java/com/wgq/chat/contact/ContactMainSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgq.chat.contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactMainSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
