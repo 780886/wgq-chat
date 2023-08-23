0ee636a97ee17e18759419b7f195e28bf8b59985
wgq <1351686755@qq.com>
2023-08-23 17:27:18 +0800

    update

--- im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
+++ im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
@@ -1,11 +1,10 @@
 package com.wgq.chat.boot;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.wgq.*","com.sheep.*"},exclude = DruidDataSourceAutoConfigure.class)
+@SpringBootApplication(scanBasePackages = {"com.wgq.*","com.sheep.*"})
 @MapperScan(basePackages = {"com.wgq.chat.dao","com.wgq.passport.dao"})
 public class ImApplication {
 
