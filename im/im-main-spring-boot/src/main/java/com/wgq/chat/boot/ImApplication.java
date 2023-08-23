7f783caf69171ece89eadc9fe3b68dab7136dad8
wgq <1351686755@qq.com>
2023-08-23 18:00:18 +0800

    update

--- im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
+++ im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
@@ -3,13 +3,17 @@ package com.wgq.chat.boot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication(scanBasePackages = {"com.wgq.*","com.sheep.*"})
 @MapperScan(basePackages = {"com.wgq.chat.dao","com.wgq.passport.dao"})
 public class ImApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(ImApplication.class, args);
+		Object userDao = applicationContext.getBean("userDao");
+		System.out.println(userDao);
 	}
 
 }
