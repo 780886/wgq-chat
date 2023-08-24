673a103095c3e20e35db94174fc6b54579b8f0a3
wgq <1351686755@qq.com>
2023-08-24 11:10:51 +0800

    mapper-locations: classpath*:mapper/*.xml   即在classpath后面加一个*号。意义是读取JAR中的XML文件

--- im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
+++ im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
@@ -1,5 +1,7 @@
 package com.wgq.chat.boot;
 
+import com.wgq.passport.dao.UserDao;
+import com.wgq.passport.po.User;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +11,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication(scanBasePackages = {"com.wgq.*","com.sheep.*"})
 @MapperScan(basePackages = {"com.wgq.chat.dao","com.wgq.passport.dao"})
 public class ImApplication {
-
+	
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(ImApplication.class, args);
-		Object userDao = applicationContext.getBean("userDao");
-		System.out.println(userDao);
+		UserDao userDao = (UserDao) applicationContext.getBean("userDao");
+		User wgq = userDao.getUserByUserName("wgq");
+		System.out.println(wgq);
 	}
 
 }
--- social-contact/contact-main-spring-boot/src/main/java/com/wgq/chat/contact/boot/ContactApplication.java
+++ social-contact/contact-main-spring-boot/src/main/java/com/wgq/chat/contact/boot/ContactApplication.java
@@ -1,21 +1,22 @@
 package com.wgq.chat.contact.boot;
 
 
-import com.wgq.chat.contact.po.Qun;
+import com.wgq.passport.dao.UserDao;
+import com.wgq.passport.po.User;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-
-import static com.wgq.sql.generator.CodeGenerator.generaCreateDDL;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.wgq.*","com.sheep.*"})
 @MapperScan(basePackages = {"com.wgq.chat.contact.dao","com.wgq.passport.dao"})
 public class ContactApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ContactApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ContactApplication.class, args);
+        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
+        User wgq = userDao.getUserByUserName("wgq");
+        System.out.println(wgq);
 //        try {
 //            generaCreateDDL(Qun.class);
 //        } catch (IOException e) {
