0d480d343fb21c76c13f9556976dd4ec9b3b9802
“wgq” <1351686755@qq.com>
2023-08-25 01:28:54 +0800

    添加群接口

--- im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
+++ im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
@@ -1,16 +1,27 @@
 package com.wgq.chat.boot;
 
+import com.wgq.passport.dao.UserDao;
+import com.wgq.passport.po.User;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-
+/**
+ * @ClassName ImApplication
+ * @Description TODO
+ * @Author wgq
+ * @Date 2023/8/24 19:21
+ * @Version 1.0
+ **/
 @SpringBootApplication(scanBasePackages = {"com.wgq.*","com.sheep.*"})
 @MapperScan(basePackages = {"com.wgq.chat.dao","com.wgq.passport.dao"})
 public class ImApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(ImApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ImApplication.class, args);
+        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
+        User wgq = userDao.getUserByUserName("wgq");
+        System.out.println("wgq = " + wgq);
+    }
 }
--- social-contact/contact-main-spring-boot/src/main/java/com/wgq/chat/contact/boot/ContactApplication.java
+++ social-contact/contact-main-spring-boot/src/main/java/com/wgq/chat/contact/boot/ContactApplication.java
@@ -1,22 +1,21 @@
 package com.wgq.chat.contact.boot;
 
 
-import com.wgq.passport.dao.UserDao;
-import com.wgq.passport.po.User;
+import com.wgq.chat.contact.po.Qun;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
+
+import static com.wgq.sql.generator.CodeGenerator.generaCreateDDL;
 
 @SpringBootApplication(scanBasePackages = {"com.wgq.*","com.sheep.*"})
 @MapperScan(basePackages = {"com.wgq.chat.contact.dao","com.wgq.passport.dao"})
 public class ContactApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(ContactApplication.class, args);
-        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
-        User wgq = userDao.getUserByUserName("wgq");
-        System.out.println(wgq);
+        SpringApplication.run(ContactApplication.class, args);
 //        try {
 //            generaCreateDDL(Qun.class);
 //        } catch (IOException e) {
