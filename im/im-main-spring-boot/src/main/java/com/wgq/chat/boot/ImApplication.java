efffd49e9bcdd23b82a979663123e538318ab17f
wgq <1351686755@qq.com>
2023-09-07 08:49:29 +0800

    mapper-locations: classpath*:mapper/*.xml   即在classpath后面加一个*号。意义是读取JAR中的XML文件

--- im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
+++ im/im-main-spring-boot/src/main/java/com/wgq/chat/boot/ImApplication.java
@@ -1,7 +1,5 @@
 package com.wgq.chat.boot;
 
-import com.wgq.passport.dao.UserDao;
-import com.wgq.passport.po.User;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,8 +18,5 @@ public class ImApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(ImApplication.class, args);
-        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
-        User wgq = userDao.getUserByUserName("wgq");
-        System.out.println("wgq = " + wgq);
     }
 }
